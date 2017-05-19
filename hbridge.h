fa67ab178fa96a4cab48f7439b1729c4976f668e
uamp <redhotrabbi@hotmail.com>
2017-05-19 23:11:23 +0100

    Update and rename dc_motor.h to hbridge.h

---
+++ hbridge.h
@@ -0,0 +1,20 @@
+#ifndef __HBRIDGE_H_INCLUDED__
+#define __HBRIDGE_H_INCLUDED__
+
+class hbridge {
+private:
+	uint8_t pin_A;
+	uint8_t pin_B;
+	uint8_t pin_enable;
+
+public:
+	hbridge(uint8_t _pin_A, uint8_t _pin_B, uint8_t _pin_enable); 
+	~hbridge();
+
+	void forward();
+	void reverse();
+	void stop();
+	void control(bool motor_on, bool direction);
+};
+
+#endif // __HBRIDGE_H_INCLUDED__ 	
